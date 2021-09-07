c06ee013489388661232c5d7a6b37dafa3ce6a1c
cemforal <35215021+cemforal@users.noreply.github.com>
2021-09-07 04:48:45 +0300

    Add project files.

---
+++ MapTo/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using MapTo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace MapTo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(List<string> data)
+        {
+            //System.IO.File.WriteAllText("ss", data.ToString());
+            TextWriter writer;
+            using (writer = new StreamWriter(@"C:\Users\Asus\source\repos\MapTo\MapTo\theFile.json", append: false))
+            {
+                writer.WriteLine(JsonConvert.SerializeObject(data));
+            }
+            return null;
+        }
+
+ 
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
